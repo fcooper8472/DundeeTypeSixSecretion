b10284b3e42c423c3f8fe25bd7bbcdea33bf7c61
Fergus Cooper <fcooper8472@gmail.com>
2017-08-10 18:47:27 +0100

    Add simple symmetric test and increase box collection size

--- test/TestCapsuleSimulation.hpp
+++ test/TestCapsuleSimulation.hpp
@@ -72,7 +72,83 @@ class TestCapsuleSimulation : public AbstractCellBasedTestSuite
 {
 public:
 
-    void TestSimpleCapsuleSimulation() throw (Exception)
+    void xTestSmallSymmetricCapsuleSimulation() throw (Exception)
+    {
+        EXIT_IF_PARALLEL;
+        // Create some capsules
+        std::vector<Node<2>*> nodes;
+
+        nodes.push_back(new Node<2>(0u, Create_c_vector(4.0, 4.0)));
+        nodes.push_back(new Node<2>(1u, Create_c_vector(4.0, 6.0)));
+        nodes.push_back(new Node<2>(2u, Create_c_vector(7.0, 4.0)));
+        nodes.push_back(new Node<2>(3u, Create_c_vector(7.0, 6.0)));
+
+
+        /*
+         * We then convert this list of nodes to a `NodesOnlyMesh`,
+         * which doesn't do very much apart from keep track of the nodes.
+         */
+        NodesOnlyMesh<2> mesh;
+        mesh.ConstructNodesWithoutMesh(nodes, 150.5);
+
+        mesh.GetNode(0u)->AddNodeAttribute(0.0);
+        mesh.GetNode(0u)->rGetNodeAttributes().resize(NA_VEC_LENGTH);
+        mesh.GetNode(0u)->rGetNodeAttributes()[NA_ANGLE] = 0.25 * M_PI;
+        mesh.GetNode(0u)->rGetNodeAttributes()[NA_LENGTH] = 2.0;
+        mesh.GetNode(0u)->rGetNodeAttributes()[NA_RADIUS] = 0.5;
+
+        mesh.GetNode(1u)->AddNodeAttribute(0.0);
+        mesh.GetNode(1u)->rGetNodeAttributes().resize(NA_VEC_LENGTH);
+        mesh.GetNode(1u)->rGetNodeAttributes()[NA_ANGLE] = -0.25 * M_PI;
+        mesh.GetNode(1u)->rGetNodeAttributes()[NA_LENGTH] = 2.0;
+        mesh.GetNode(1u)->rGetNodeAttributes()[NA_RADIUS] = 0.5;
+
+        mesh.GetNode(2u)->AddNodeAttribute(0.0);
+        mesh.GetNode(2u)->rGetNodeAttributes().resize(NA_VEC_LENGTH);
+        mesh.GetNode(2u)->rGetNodeAttributes()[NA_ANGLE] = -0.25 * M_PI;
+        mesh.GetNode(2u)->rGetNodeAttributes()[NA_LENGTH] = 2.0;
+        mesh.GetNode(2u)->rGetNodeAttributes()[NA_RADIUS] = 0.5;
+
+        mesh.GetNode(3u)->AddNodeAttribute(0.0);
+        mesh.GetNode(3u)->rGetNodeAttributes().resize(NA_VEC_LENGTH);
+        mesh.GetNode(3u)->rGetNodeAttributes()[NA_ANGLE] = 0.25 * M_PI;
+        mesh.GetNode(3u)->rGetNodeAttributes()[NA_LENGTH] = 2.0;
+        mesh.GetNode(3u)->rGetNodeAttributes()[NA_RADIUS] = 0.5;
+
+        //Create cells
+        std::vector<CellPtr> cells;
+        auto p_diff_type = boost::make_shared<DifferentiatedCellProliferativeType>();
+        CellsGenerator<NoCellCycleModel, 2> cells_generator;
+        cells_generator.GenerateBasicRandom(cells, mesh.GetNumNodes(), p_diff_type);
+
+        // Create cell population
+        NodeBasedCellPopulation<2> population(mesh, cells);
+
+        population.AddCellWriter<CapsuleOrientationWriter>();
+        population.AddCellWriter<CapsuleScalingWriter>();
+
+        // Create simulation
+        OffLatticeSimulation<2> simulator(population);
+        simulator.SetOutputDirectory("CapsuleSimulation");
+        simulator.SetDt(1.0/1200.0);
+        simulator.SetSamplingTimestepMultiple(1u);
+
+        auto p_numerical_method = boost::make_shared<ForwardEulerNumericalMethodForCapsules<2,2>>();
+        simulator.SetNumericalMethod(p_numerical_method);
+
+        /*
+         * We now create a force law and pass it to the simulation
+         * We use linear springs between cells up to a maximum of 1.5 ('relaxed' cell diameters) apart, and add this to the simulation class.
+         */
+        auto p_calsule_force = boost::make_shared<CapsuleForce<2>>();
+        simulator.AddForce(p_calsule_force);
+
+        /* We then set an end time and run the simulation */
+        simulator.SetEndTime(100.0/1200.0);
+        simulator.Solve();
+    }
+
+    void TestLongerCapsuleSimulation() throw (Exception)
     {
         EXIT_IF_PARALLEL;
 
@@ -110,7 +186,7 @@ public:
          * which doesn't do very much apart from keep track of the nodes.
          */
         NodesOnlyMesh<2> mesh;
-        mesh.ConstructNodesWithoutMesh(nodes, 1.5);
+        mesh.ConstructNodesWithoutMesh(nodes, 10.0);
 
         mesh.GetNode(0u)->AddNodeAttribute(0.0);
         mesh.GetNode(0u)->rGetNodeAttributes().resize(NA_VEC_LENGTH);
@@ -159,7 +235,7 @@ public:
 //        simulator.AddCellPopulationBoundaryCondition(p_boundary_condition);
 
         /* We then set an end time and run the simulation */
-        simulator.SetEndTime(1.0);
+        simulator.SetEndTime(150.0/1200.0);
         simulator.Solve();
     }
 };
